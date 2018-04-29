66c7b0b4d4b4801c53b9b3620ee4a12c09f52088
Eshan Halekote <ehalekote@eshans-mbp-3.attlocal.net>
2018-04-29 17:45:47 -0500

    started main()

--- cheaters.java
+++ cheaters.java
@@ -1,6 +1,30 @@
-
+import java.io.File;
+		
 public class cheaters {
 	public static void main(String args[]) {
-		//Read in command line arguments
+		int[][] commonPhrases;		//Matrix to hold number of common words between row and column numbered essays
+		
+		File dir = new File("/Users/ehalekote/Desktop/cheaters_documents/sm_doc_set"); //Set to appropriate directory path
+		File[] directoryListing = dir.listFiles();
+		
+		//Fine number of files
+		int matrixCount = 0;
+		if (directoryListing != null) {
+		  for (File essay : directoryListing) {
+			  matrixCount++;
+		  }
+		}
+		
+		//Iterate through files
+		if (directoryListing != null) {
+		  for (File essay : directoryListing) {
+		      //create hashmap of terms
+			  //compare every other essay to this one
+			  //store in a global matrix
+		  }
+		}
+  
 	}
+	
+
 }
